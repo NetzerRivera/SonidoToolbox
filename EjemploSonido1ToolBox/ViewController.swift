628eb04831b37c9a0d548ced247d2deee8568add
NetzerRivera <netzerrivera@gmail.com>
2015-12-21 17:13:05 -0600

    Ver 1

--- EjemploSonido1ToolBox/ViewController.swift
+++ EjemploSonido1ToolBox/ViewController.swift
@@ -6,20 +6,43 @@
 //  Copyright © 2015 Netzer Rivera. All rights reserved.
 //
 
+// Sonido Simple
+
 import UIKit
+import AudioToolbox
+
 
 class ViewController: UIViewController {
 
+    private var miSonido:SystemSoundID = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let sonidoURL = NSBundle.mainBundle().URLForResource("Ping", withExtension: "aiff")
+        AudioServicesCreateSystemSoundID(sonidoURL! as CFURL, &miSonido)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+
     }
 
+    @IBAction func play() {
+        
+        AudioServicesPlaySystemSound(miSonido)
+        
+    }
+    
+    
+    
+    
+    
 
 }
 
